04609b98b23693f54f1c4fba163d050771101c23
alexandra chase <amchas26@g.holycross.edu>
2024-10-25 18:43:06 -0400

    try this

--- webserver.py
+++ webserver.py
@@ -697,8 +697,11 @@ def handle_http_post_like(req,conn):
         topicIndex = data.topics.index(topic)               # get the index of this topic in the list of topics
         if topicIndex == -1:
             return Response("404 NOT FOUND", "text/plain", "Never heard of this topic!")
+    else:
+        print("There is version number.")
 
     with data.lock:
+        topicIndex = data.topics.index(topic)               # get the index of this topic in the list of topics
         data.likes[topicIndex] += 1                         # increment the number of likes corresponding with that topic
         data.versHome += 1                                  # update the home feed ver number
         data.lock.notify_all()                              # notify everyone
